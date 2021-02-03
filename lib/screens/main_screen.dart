3447d543fee773d83924062c4dbdd9c02953e52f
asifimtiazahmed <a.imtiaz@gmail.com>
2021-02-03 18:22:14 -0500

    Appbar almost done basic ToDo list created

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../services/const.dart';
+
 class MainScreen extends StatefulWidget {
   @override
   _MainScreenState createState() => _MainScreenState();
@@ -12,9 +14,47 @@ class _MainScreenState extends State<MainScreen> {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          title: Text('Ultimate Tasbih'),
+          leading: Builder(
+            builder: (BuildContext context) {
+              return IconButton(
+                icon: const Icon(Icons.menu, color: cornSilk, size: iconSize),
+                onPressed: () { Scaffold.of(context).openDrawer(); },
+                tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+              );
+            },
+          ),
+          title: Text('Ultimate Tasbih',
+            style: TextStyle(
+              color: cornSilk,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+          backgroundColor: kombuGreen,
+          shadowColor: oliveGreen,
+          elevation: 9,
+          actions: [
+                  IconButton(
+                    padding: EdgeInsets.fromLTRB(0,0,18,0),
+                    icon: Icon (Icons.account_circle,
+                    size: iconSize,),
+                    color: cornSilk,
+                    onPressed: ()=> null,
+                  ),
+          ],
+
         ),
       ),
     );
   }
 }
+
+// TODO: change background color to cornSilk
+// TODO: import neumorphic package
+// TODO: import the splash type progress indicator
+// TODO: import storage package to keep the count in memory
+// TODO: Shift App bar to its own widget area
+// TODO: Create a column to incorporate the dua space, counter, button
+// TODO: Add in Settings left handed or Right handed
+// TODO: Import Google font for most typing, counter
+// TODO: implement the screen size MediaQery for responsiveness
---
+++ lib/services/const.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kombuGreen = Color(0XFF283618); //Kombu Green
+const oliveGreen = Color(0XFF606C38); //Dark Olive Green
+const cornSilk = Color(0XFFFEFAE0); //Corn Silk
+const fawn = Color(0XFFDDA15E); //Fawn
+const liverDogs = Color(0XFFBC6C25);
+const double iconSize = 35.0; //Liver Dogs
